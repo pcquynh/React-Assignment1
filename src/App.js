bc6eeb533d7669bdad69bd1fa939851885232038
pcquynh <pcquynh@mun.ca>
2022-05-13 16:37:44 -0230

    Display movie information in Home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from "react";
 import "./App.css";
+import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import { Home, AddReview } from "./pages";
 
 function App() {
+  const [movies, setMovies] = useState(null);
+  useEffect(() => {
+    // load the json data
+    fetch("./movies.json")
+      .then((response) => response.json())
+      .then(setMovies)
+      .catch((e) => console.log(e.message));
+  }, []);
+
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={<Home movies={movies} setMovies={setMovies} />}
+        />
         <Route path="/addReview" element={<AddReview />} />
       </Routes>
     </div>
